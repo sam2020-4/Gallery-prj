d0ff52eb3971df54d97f89f3a5427ced78c493ca
matata samuel <mattasamuel3@gmail.com>
2020-10-10 14:19:41 +0300

    added category class methods

--- pics/models.py
+++ pics/models.py
@@ -3,10 +3,14 @@ from django.db import models
 # Create your models here.
 # db relashionships
 class Category(models.Model):
-    name = models.CharField(max_length =30)
+    category_name = models.CharField(max_length =30)
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
     def __str__(self):
-        return self.name
-
-
+        return self.category_name
 
