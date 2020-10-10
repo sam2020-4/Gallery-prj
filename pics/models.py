4f6d34e72866821c227cd8e740a17f9a4147680f
matata samuel <mattasamuel3@gmail.com>
2020-10-10 14:29:24 +0300

    added a delete image function

--- pics/models.py
+++ pics/models.py
@@ -24,7 +24,10 @@ class Image(models.Model):
 
     def save_image(self):
         self.save()
-        
+    
+    def delete_image(self):
+        self.delete()
+
     def __str__(self):
         return self.image_name
 
