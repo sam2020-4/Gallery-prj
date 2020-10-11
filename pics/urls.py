db91d72c3ffc8084bae3cfd1268ec2fbc1c99b0a
matata samuel <mattasamuel3@gmail.com>
2020-10-11 07:15:06 +0300

    changed welcome urls to index

--- pics/urls.py
+++ pics/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.index, name = 'welcome'),
-    # url(r'^search/', views.search_results, name='search_results'),
+    url('^$',views.index, name = 'index'),
+    # url(r'^search/', views.search_results, name='search_results')
 ] 
