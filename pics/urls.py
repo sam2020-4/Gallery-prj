c724e2a047779687f30f0bfc1019521fdf0af5a3
matata samuel <mattasamuel3@gmail.com>
2020-10-11 07:21:12 +0300

    inserted search template

--- pics/urls.py
+++ pics/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 
+
 urlpatterns=[
     url('^$',views.index, name = 'index'),
     url(r'^gallery$', views.gallery, name='gallery'),
