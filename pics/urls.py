17986cc3507b03faa272a2c186b5d6ea97706fcc
matata samuel <mattasamuel3@gmail.com>
2020-10-11 07:16:45 +0300

    inserted gallery url on application

--- pics/urls.py
+++ pics/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index, name = 'index'),
+    url(r'^gallery$', views.gallery, name='gallery'),
     # url(r'^search/', views.search_results, name='search_results')
-] 
+]     
+   
